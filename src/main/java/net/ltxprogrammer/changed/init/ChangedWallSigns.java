f30795031ab046f37584eaa2ab8887001fb165df
LtxPgm <ltxprogrammer@outlook.com>
2026-01-27 16:08:59 -0600

    Made the tall wall signs bigger.
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/init/ChangedWallSigns.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedWallSigns.java
@@ -20,6 +20,6 @@ public class ChangedWallSigns {
     public static final RegistryObject<WallSignVariant> SQUID = register(32, 24, "squid");
     public static final RegistryObject<WallSignVariant> PROTOTYPE = register(32, 24, "prototype");
     public static final RegistryObject<WallSignVariant> CAT = register(32, 24, "cat");
-    public static final RegistryObject<WallSignVariant> DO_NOT_TOUCH_LATEXES = register(16, 32, "do_not_touch_latexes");
-    public static final RegistryObject<WallSignVariant> DO_NOT_SPEAK_WITH_DARK_LATEXES = register(16, 32, "do_not_speak_with_dark_latexes");
+    public static final RegistryObject<WallSignVariant> DO_NOT_TOUCH_LATEXES = register(20, 40, "do_not_touch_latexes");
+    public static final RegistryObject<WallSignVariant> DO_NOT_SPEAK_WITH_DARK_LATEXES = register(20, 40, "do_not_speak_with_dark_latexes");
 }
