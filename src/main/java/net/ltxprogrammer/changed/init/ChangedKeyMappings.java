7ec300e7b332482e494121ee7e03836b7e629a05
LtxPgm <ltxprogrammer@outlook.com>
2026-02-04 22:55:25 -0600

    Removed ability requirement to be outside a menu. Modernized key mapping impl. Fixed keybinds not releasing on opening GUI. Fixed #1639
    Signed-off-by: LtxPgm <ltxprogrammer@outlook.com>

--- src/main/java/net/ltxprogrammer/changed/init/ChangedKeyMappings.java
+++ src/main/java/net/ltxprogrammer/changed/init/ChangedKeyMappings.java
@@ -1,5 +1,6 @@
 package net.ltxprogrammer.changed.init;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.ltxprogrammer.changed.Changed;
 import net.ltxprogrammer.changed.ability.GrabEntityAbility;
 import net.ltxprogrammer.changed.network.ExtraJumpKeybind;
@@ -13,14 +14,59 @@ import net.minecraft.client.player.LocalPlayer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.glfw.GLFW;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
 public class ChangedKeyMappings {
-    public static final KeyMapping SELECT_ABILITY = new KeyMapping("key.changed.variant_ability", GLFW.GLFW_KEY_R, "key.categories.ui");
-    public static final KeyMapping USE_ABILITY = new KeyMapping("key.changed.use_ability", GLFW.GLFW_KEY_Z, "key.categories.movement");
+    public static final KeyMapping SELECT_ABILITY = new KeyMapping("key.changed.variant_ability",
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_R, "key.categories.ui") {
+        @Override
+        public void setDown(boolean newState) {
+            super.setDown(newState);
+            if (!newState) return;
+
+            LocalPlayer local = Minecraft.getInstance().player;
+            GrabEntityAbility.getGrabberSafe(local).ifPresent(entity -> {
+                if (entity.getAbilityInstanceSafe(ChangedAbilities.GRAB_ENTITY_ABILITY.get())
+                        .map(ability -> ability.grabbedHasControl).orElse(false))
+                    Changed.PACKET_HANDLER.sendToServer(VariantAbilityActivate.openRadial(local));
+            });
+
+            ProcessTransfur.ifPlayerTransfurred(local, variant -> {
+                if (variant.isTemporaryFromSuit())
+                    return;
+
+                Changed.PACKET_HANDLER.sendToServer(VariantAbilityActivate.openRadial(local));
+                ChangedTutorial.triggerOnOpenRadial();
+            });
+        }
+    };
+    public static final KeyMapping USE_ABILITY = new KeyMapping("key.changed.use_ability",
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_Z, "key.categories.movement") {
+        @Override
+        public void setDown(boolean newState) {
+            super.setDown(newState);
+            LocalPlayer local = Minecraft.getInstance().player;
+            ProcessTransfur.ifPlayerTransfurred(local, variant -> {
+                assert local != null;
+                if (variant.isTemporaryFromSuit())
+                    return;
+
+                // KeyStateTracker will check if the state has changed
+                if (variant.abilityKey.queueKeyState(newState)) {
+                    ChangedTutorial.triggerOnUseAbility(variant.getSelectedAbility());
+                    Changed.PACKET_HANDLER.sendToServer(new VariantAbilityActivate(local, newState, variant.selectedAbility));
+                }
+            });
+        }
+    };
 
     @SubscribeEvent
     public static void registerKeyBindings(RegisterKeyMappingsEvent event) {
@@ -38,38 +84,6 @@ public class ChangedKeyMappings {
                 return;
 
             if (Minecraft.getInstance().screen == null) {
-                if (event.getKey() == USE_ABILITY.getKey().getValue()) {
-                    USE_ABILITY.consumeClick();
-
-                    ProcessTransfur.ifPlayerTransfurred(local, variant -> {
-                        if (variant.isTemporaryFromSuit())
-                            return;
-
-                        var newState = event.getAction() != GLFW.GLFW_RELEASE;
-                        if (variant.abilityKey.queueKeyState(newState)) {
-                            ChangedTutorial.triggerOnUseAbility(variant.getSelectedAbility());
-                            Changed.PACKET_HANDLER.sendToServer(new VariantAbilityActivate(local, newState, variant.selectedAbility));
-                        }
-                    });
-                }
-
-                if (event.getKey() == SELECT_ABILITY.getKey().getValue() && event.getAction() == GLFW.GLFW_PRESS) {
-                    SELECT_ABILITY.consumeClick();
-                    GrabEntityAbility.getGrabberSafe(local).ifPresent(entity -> {
-                        if (entity.getAbilityInstanceSafe(ChangedAbilities.GRAB_ENTITY_ABILITY.get())
-                                .map(ability -> ability.grabbedHasControl).orElse(false))
-                            Changed.PACKET_HANDLER.sendToServer(VariantAbilityActivate.openRadial(local));
-                    });
-
-                    ProcessTransfur.ifPlayerTransfurred(local, variant -> {
-                        if (variant.isTemporaryFromSuit())
-                            return;
-
-                        Changed.PACKET_HANDLER.sendToServer(VariantAbilityActivate.openRadial(local));
-                        ChangedTutorial.triggerOnOpenRadial();
-                    });
-                }
-
                 if (event.getKey() == options.keyJump.getKey().getValue() && event.getAction() == GLFW.GLFW_PRESS) {
                     if (!local.onGround())
                         ProcessTransfur.ifPlayerTransfurred(local, variant -> {
